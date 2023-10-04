f3b984ba04fb423f54bfd12eac7a93904fdb630e
sydneyowl <pauseplay@126.com>
2023-10-04 10:28:42 +0800

    rename *cmd

--- entrance/consts.go
+++ entrance/consts.go
@@ -1,4 +1,4 @@
-package cmd
+package entrance
 
 var available_choices = []string{
 	"信道", "发射允许", "接收频率", "QT/DTQ解码(接收哑音)", "发射频率", "QT/DQT编码（发射亚音）", "发射功率", "带宽", "PTT-ID", "繁忙锁定", "扫描添加", "信令码", "信道名称", "加密",
--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/sydneyowl/shx8800-config-editor/cmd"
+	"github.com/sydneyowl/shx8800-config-editor/entrance"
 	"os"
 )
 
 func main() {
 	cobra.MousetrapHelpText = ""
-	if err := cmd.BaseCmd.Execute(); err != nil {
+	if err := entrance.BaseCmd.Execute(); err != nil {
 		fmt.Printf("程序无法启动: %v", err)
 		os.Exit(-1)
 	}
